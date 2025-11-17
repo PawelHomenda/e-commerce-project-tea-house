d818f7ed09d69eaa3bf446ee08ef48ec30583541
Kage-Bushi (Fedora) <pawelhomenda@outlook.es>
2025-11-17 13:03:46 +0100

    Rearanged and springboot added

---
+++ api_service/src/main/java/es/kohchiku_bayashi/e_commerce_teahouse/ECommerceTeahouseApplication.java
@@ -0,0 +1,13 @@
+package es.kohchiku_bayashi.e_commerce_teahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceTeahouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceTeahouseApplication.class, args);
+	}
+
+}
---
+++ api_service/src/test/java/es/kohchiku_bayashi/e_commerce_teahouse/ECommerceTeahouseApplicationTests.java
@@ -0,0 +1,13 @@
+package es.kohchiku_bayashi.e_commerce_teahouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTeahouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
